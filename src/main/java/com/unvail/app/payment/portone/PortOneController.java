44eaaf3ab28d77e2cab360904c4644ee4d64b8b8
hjw1456 <hjw1456@gmail.com>
2026-01-17 20:14:19 +0900

    내용 수정

--- src/main/java/com/unvail/app/payment/portone/PortOneController.java
+++ src/main/java/com/unvail/app/payment/portone/PortOneController.java
@@ -2,6 +2,7 @@ package com.unvail.app.payment.portone;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.json.Json;
 import com.unvail.app.comm.ContextUtils;
 import com.unvail.app.comm.error.BusinessException;
 import com.unvail.app.comm.error.ErrorCode;
@@ -10,12 +11,14 @@ import io.portone.sdk.server.payment.Payment;
 import io.portone.sdk.server.payment.PaymentWebhookRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Slf4j
@@ -55,7 +58,16 @@ public class PortOneController {
     }
 
     @PostMapping("/{provider}/complete/webhook")
-    public void paymentCompleteWebhook(@RequestBody PaymentWebhookRequest request) {
-        log.debug("paymentCompleteWebhook= {}", request);
+    public ResponseEntity<Void> paymentCompleteWebhook(
+            @RequestBody String rawBody,
+            @RequestHeader Map<String, String> headers
+    ) {
+        log.debug("raw webhook body = {}", rawBody);
+
+        // 1. 시그니처 검증
+        // 2. JSON 파싱
+        // 3. 비즈니스 처리
+
+        return ResponseEntity.ok().build();
     }
 }
